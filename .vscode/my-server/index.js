1f9e6a9e2989ed31033cba4a572334f54be78be7
emptyboy84 <emptyboy85@gmail.com>
2025-11-17 17:43:32 +0900

    e

--- .vscode/my-server/index.js
+++ .vscode/my-server/index.js
@@ -1,5 +1,12 @@
-function calculateSum(a, b) {
-  return a + b;
-}
+function main(){
+      const varible = 15;
+      const varible2='goal';
+      const statement=`${varible} is ${varible2}`;
+      console.log(`${varible} is ${varible2}`);
+      console.log('name:%s','홍길동');
+      console.log('name:%d',12.65);
+      console.log(statement);
 
-module.exports = { calculateSum };
+return statement;
+}
+module.exports={main};
--- .vscode/my-server/server.js
+++ .vscode/my-server/server.js
@@ -4,24 +4,8 @@ const PORT = 5001;
 
 app.use(express.static(__dirname));
 
-// index.js에서 함수 가져오기
-const { calculateSum } = require('./index.js');
-
-// 결과를 보여주는 페이지
 app.get('/', (req, res) => {
-  const result = calculateSum(5, 3);
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>결과</title>
-    </head>
-    <body>
-        <h1>계산 결과</h1>
-        <p>5 + 3 = <strong>${result}</strong></p>
-    </body>
-    </html>
-  `);
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, '127.0.0.1', () => {
